5622a6fa0f81fd68f2af9da4ce2d80c2e4fc9194
hachan <kh.linuxx@gmail.com>
2024-02-20 04:27:21 +0900

    Feb-20,2024

---
+++ rop2-api/files/rop2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <err.h>
+
+int main()
+{
+    setbuf(stdout,NULL);
+    char buf[0x20];
+    printf("Can you hack my shell?");
+    gets(buf);
+    return 0;
+}
+
+
+__attribute__((constructor))
+void init() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    alarm(60);
+}
